31f8103ae4691d0716b01f9bfc0a57e666cfc9b9
zf2timo <timo.tewes@gmail.com>
2016-09-28 22:12:13 +0200

    Changed publish file to own workflow

--- publish.sh
+++ publish.sh
@@ -4,10 +4,14 @@
 # was downloaded and placed in this directory instead of sculpin having been
 # installed globally.
 
-sculpin generate --env=prod
+./vendor/bin/sculpin generate --env=prod
 if [ $? -ne 0 ]; then echo "Could not generate the site"; exit 1; fi
 
 # Add --delete right before "output_prod" to have rsync remove files that are
 # deleted locally from the destination too. See README.md for an example.
-rsync -avze 'ssh -p 4668' output_prod/ username@yoursculpinsite:public_html
+rsync -avze --delete output_prod/ docs/
 if [ $? -ne 0 ]; then echo "Could not publish the site"; exit 1; fi
+
+git add docs/
+git commit -m"Updated Blog"
+git push origin master
